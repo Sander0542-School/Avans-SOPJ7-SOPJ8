246e76203401eace73534b5b2a9aada677763bfc
Duwang <45719439+WhatABeatifulDuwang@users.noreply.github.com>
2021-03-15 13:13:03 +0100

    Changed Controller to match Layer Controller

--- app/Http/Controllers/SubjectController.php
+++ app/Http/Controllers/SubjectController.php
@@ -2,46 +2,129 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Resources\SubjectResource;
 use App\Models\Subject;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
 
 class SubjectController extends Controller
 {
-    public function Index(){
-        foreach (Subject::all() as $subject){
-            return $subject->name;
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $subjectArray =[];
+        foreach (Subject::all() as $subject) {
+             array_push($subjectArray, ['id'=>$subject->id, 'name'=>$subject->name]);
+        }
+
+        return response()->json($subjectArray);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        // later toegevoegd
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $domain_id = $request->domain_id;
+        $name = $request->name;
+        $created_at = Carbon::now();
+        $updated_at = $created_at;
+
+        if (Subject::where('name', $name)->exists()) {
+            return response()->json(['Error' => 'Subject \''.$name.'\' already exists.'])->setStatusCode(400);
+        }
+
+        $newSubjectObject = Subject::create([
+            'domain_id'=> $domain_id,
+            'name' => $name,
+            'created_at' => $created_at,
+            'updated_at' => $updated_at,
+        ]);
+
+        if ($newSubjectObject->save()) {
+            return response()->json(['Message'=> 'Subject \''.$name.'\' created!'])->setStatusCode(201);
+        } else {
+            return response()->json(['Error'=> 'Subject \''.$name.'\' could not be created.'])->setStatusCode(400);
         }
     }
 
-    public function Find(Subject $subject){
-        return $subject = Subject::where('domain_id', domainId)
-                            ->orderBy('name')
-                            ->get();
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Subject  $subject
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return new SubjectResource(Subject::findOrFail($id));
     }
 
-    public function Store(Request $request){
-        $subject = new Subject;
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Subject  $subject
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Subject $subject)
+    {
+        // later toegevoegd
+    }
 
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Subject  $subject
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $subject = Subject::findOrFail($id);
+
+        $subject->id = $request->id;
+        $subject->domain_id = $request->domain_id;
         $subject->name = $request->name;
+        $subject->created_at = $request->created_at;
+        $subject->updated_at = $request->updated_at;
 
-        $subject->save();
-
-        //$subject = subject.create([
-        //    'name' => 'Lorem Ipsum'
-        //    ]);
+        if ($subject->save()) {
+            return response()->json(['Message' => 'Subject with id: \''.$id.'\' updated!'])->setStatusCode(200);
+        } else {
+            return response()->json(['Error' => 'Subject with id: \''.$id.'\' could not be updated.'])->setStatusCode(400);
+        }
     }
 
-    public function Update(Subject $subject){
-        $subject = Subject::find(1);
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Subject  $subject
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $subject = Subject::findOrFail($id);
 
-        $subject->name = 'Lorem Ipsum';
-
-        $subject->save();
-    }
-
-    public function Delete(Subject $subject){
-        $subject = Subject::find(1);
-
-        $subject->delete();
+        if ($subject->delete()) {
+            return response()->json(['Message' => 'Subject with id: \''.$id.'\' deleted!'])->setStatusCode(200);
+        } else {
+            return response()->json(['Error' => 'Subject with id: \''.$id.'\' could not be deleted.'])->setStatusCode(400);
+        }
     }
 }
