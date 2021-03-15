5299a5ee9f3ef66d86bbdcc7b264394d17dc2f57
Joren <63969493+joren961@users.noreply.github.com>
2021-03-15 17:47:35 +0100

    added Stamen option, zoomcontrol fixed

--- public/js/app.js
+++ public/js/app.js
@@ -3898,16 +3898,29 @@ window.toggleSideMenu = function () {
 /***/ (() => {
 
 var layerTemplate = "http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png";
+var southWest = L.latLng(52.12606258692764, 6.474997008464673),
+    northEast = L.latLng(52.134743339394106, 6.501807460723494),
+    bounds = L.latLngBounds(southWest, northEast);
 var map = Leaflet.map('mapid', {
   minZoom: 16,
-  maxZoom: 18
+  maxZoom: 19,
+  zoomControl: false,
+  maxBounds: bounds
 }).setView([52.1305, 6.4893], 16);
-map.setMaxBounds(map.getBounds());
+map.scrollWheelZoom.disable();
 Leaflet.tileLayer(layerTemplate, {
-  maxZoom: 18,
+  maxZoom: 19,
   minZoom: 16
-}).addTo(map);
-window.subjectMap = map; // $('mapid').on("mousedown", Leaflet.DomEvent.stopPropagation);
+}).addTo(map); // const StamenTemplate = "http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.jpg";
+// const StamenMap = Leaflet.map('mapid', {minZoom: 12, maxZoom: 14, zoomControl: false}).setView([41.055408357430274, -95.88192848817997],12)
+// StamenMap.zoomControl = false;
+// StamenMap.dragging.disable();
+// Leaflet.tileLayer(StamenTemplate, {
+//     maxZoom: 14,
+//     minZoom: 12
+// }).addTo(StamenMap);
+
+window.subjectMap = StamenMap;
 
 /***/ }),
 
--- resources/js/subjectmap.js
+++ resources/js/subjectmap.js
@@ -1,14 +1,22 @@
 const layerTemplate = "http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png";
-
-const map = Leaflet.map('mapid', {minZoom: 16, maxZoom: 18}).setView([52.1305, 6.4893], 16);
-
-map.setMaxBounds(map.getBounds());
-
+const southWest = L.latLng(52.12606258692764, 6.474997008464673),
+    northEast = L.latLng(52.134743339394106, 6.501807460723494),
+    bounds = L.latLngBounds(southWest, northEast);
+const map = Leaflet.map('mapid', {minZoom: 16, maxZoom: 19, zoomControl: false, maxBounds: bounds}).setView([52.1305, 6.4893], 16);
+map.scrollWheelZoom.disable();
 Leaflet.tileLayer(layerTemplate, {
-    maxZoom: 18,
+    maxZoom: 19,
     minZoom: 16
 }).addTo(map);
 
-window.subjectMap = map;
+// const StamenTemplate = "http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.jpg";
+// const StamenMap = Leaflet.map('mapid', {minZoom: 12, maxZoom: 14, zoomControl: false}).setView([41.055408357430274, -95.88192848817997],12)
+// StamenMap.zoomControl = false;
+// StamenMap.dragging.disable();
+// Leaflet.tileLayer(StamenTemplate, {
+//     maxZoom: 14,
+//     minZoom: 12
+// }).addTo(StamenMap);
 
-// $('mapid').on("mousedown", Leaflet.DomEvent.stopPropagation);
+window.subjectMap = StamenMap;
+
