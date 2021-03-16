ede805ab2bfbb46d62d0d359c4466b0aa3c94fb4
Duwang <45719439+WhatABeatifulDuwang@users.noreply.github.com>
2021-03-16 12:46:20 +0100

    Added validator, updated class based on LayerController

--- app/Http/Controllers/SubjectController.php
+++ app/Http/Controllers/SubjectController.php
@@ -4,8 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\SubjectResource;
 use App\Models\Subject;
-use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 
 class SubjectController extends Controller
 {
@@ -25,16 +26,6 @@ class SubjectController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        // later toegevoegd
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -42,26 +33,24 @@ class SubjectController extends Controller
      */
     public function store(Request $request)
     {
-        $domain_id = $request->domain_id;
-        $name = $request->name;
-        $created_at = Carbon::now();
-        $updated_at = $created_at;
+        $validator = Validator::make($request -> all(), [
+            'domain_id' => 'required',
+            'name' => 'required|unique:subjects,name',
+        ]);
 
-        if (Subject::where('name', $name)->exists()) {
-            return response()->json(['Error' => 'Subject \''.$name.'\' already exists.'])->setStatusCode(400);
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()])->setStatusCode(400);
         }
 
         $newSubjectObject = Subject::create([
-            'domain_id'=> $domain_id,
-            'name' => $name,
-            'created_at' => $created_at,
-            'updated_at' => $updated_at,
+            'domain_id'=> $request->domain_id,
+            'name' => $request->name,
         ]);
 
         if ($newSubjectObject->save()) {
-            return response()->json(['Message'=> 'Subject \''.$name.'\' created!'])->setStatusCode(201);
+            return new SubjectResource($newSubjectObject);
         } else {
-            return response()->json(['Error'=> 'Subject \''.$name.'\' could not be created.'])->setStatusCode(400);
+            return response()->json(['error'=> 'Subject "'.$request->name.'" could not be created.'])->setStatusCode(500);
         }
     }
 
@@ -69,44 +58,37 @@ class SubjectController extends Controller
      * Display the specified resource.
      *
      * @param  \App\Models\Subject  $subject
-     * @return \Illuminate\Http\Response
+     * @return SubjectResource
      */
     public function show($id)
     {
-        return new SubjectResource(Subject::findOrFail($id));
-    }
+        $subject = Subject::where('id', $id)->findOrFail($id);
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Subject  $subject
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Subject $subject)
-    {
-        // later toegevoegd
+        if (is_null($subject)) {
+            return response()->json(['message' => 'Model not found'])->setStatusCode(404);
+        }
+
+        return new SubjectResource($subject);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Subject  $subject
-     * @return \Illuminate\Http\Response
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
+     * @return SubjectResource
      */
     public function update(Request $request, $id)
     {
-        DB::table('subjects')
-            ->where('id', $id)
-            ->update([
-                'domain_id' => $request->domain_id,
-                'name' => $request->name,
-                'updated_at' => Carbon::now()
-            ]);
+        DB::table('subjects')->where('id', $id)->update($request->all());
 
         $updatedSubject = Subject::where('id', $id)->firstOrFail();
 
-        return new LayerResource($updatedSubject);
+        if (is_null($updatedSubject)) {
+            return response()->json(['message' => 'Model not found'])->setStatusCode(404);
+        }
+
+        return new SubjectResource($updatedSubject);
     }
 
     /**
@@ -117,12 +99,12 @@ class SubjectController extends Controller
      */
     public function destroy($id)
     {
-        $subject = Subject::findOrFail($id);
+        $subject = Subject::where('id', $id)->findOrFail($id);
 
         if ($subject->delete()) {
-            return response()->json(['Message' => 'Subject with id: \''.$id.'\' deleted!'])->setStatusCode(200);
+            return response()->json(['message' => 'Subject with id: "'.$id.'" deleted!'])->setStatusCode(200);
         } else {
-            return response()->json(['Error' => 'Subject with id: \''.$id.'\' could not be deleted.'])->setStatusCode(500);
+            return response()->json(['error' => 'Subject with id: "'.$id.'" could not be deleted.'])->setStatusCode(500);
         }
     }
 }
