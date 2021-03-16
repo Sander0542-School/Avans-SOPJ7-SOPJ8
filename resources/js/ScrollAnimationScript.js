5bb95394d0171c15d674a397358ea1763084eafd
Fedor <Fedor@DESKTOP-C8VMLEN>
2021-03-16 16:22:43 +0100

    Fixed issue where the backimage was appearing first

--- resources/js/ScrollAnimationScript.js
+++ resources/js/ScrollAnimationScript.js
@@ -1,9 +1,12 @@
 document.addEventListener('keydown', function(event){
 
+
+
     if(event.keyCode == 38)
     {
         let frontImage = document.querySelector(".frontIMG");
         let backImage = document.querySelector(".fadeObject");
+
         frontImage.classList.toggle('transition');
         backImage.classList.toggle('transition');
 
