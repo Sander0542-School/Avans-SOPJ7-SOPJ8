72523a40a8ec04250e818f5553bee360197b90ef
Andries van Rijn <andries.van.rijn@hotmail.com>
2021-03-16 11:24:28 +0100

    Changes to LayerController after review

--- app/Http/Controllers/LayerController.php
+++ app/Http/Controllers/LayerController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\LayerResource;
 use App\Models\Layer;
-use http\Env\Response;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
@@ -44,7 +42,7 @@ class LayerController extends Controller
         if ($newLayerObject->save()) {
             return new LayerResource($newLayerObject);
         } else {
-            return response()->json(['error' => 'Layer \''.$request->name.'\' could not be created.'])->setStatusCode(500);
+            return response()->json(['error' => 'Layer "'.$request->name.'" could not be created.'])->setStatusCode(500);
         }
     }
 
@@ -55,23 +53,6 @@ class LayerController extends Controller
      * @return LayerResource
      */
     public function show(string $slug)
-    {
-        $layer = Layer::where('slug', $slug)->first();
-
-        if (is_null($layer)) {
-            return response()->json(['message' => 'Model not found'])->setStatusCode(404);
-        }
-
-        return new LayerResource($layer);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param string $slug
-     * @return LayerResource
-     */
-    public function edit(string $slug)
     {
         $layer = Layer::where('slug', $slug)->first();
 
@@ -109,9 +90,9 @@ class LayerController extends Controller
         $layerToDestroy = Layer::where('slug', $slug)->firstOrFail();
 
         if ($layerToDestroy->delete()) {
-            return response()->json(['message' => 'Layer \''.$slug.'\' has been deleted.'])->setStatusCode(200);
+            return response()->json(['message' => 'Layer "'.$slug.'" has been deleted.'])->setStatusCode(200);
         } else {
-            return response()->json(['message' => 'Layer \''.$slug.'\' could not deleted.'])->setStatusCode(500);
+            return response()->json(['message' => 'Layer "'.$slug.'" could not deleted.'])->setStatusCode(500);
         }
     }
 }
