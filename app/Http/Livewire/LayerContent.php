c0c3284e6da419c8a436ddcb56969739cf560bf3
Sander Jochems <sanderjochems@hotmail.nl>
2021-03-17 17:54:42 +0100

    Render LayerContent Component

--- app/Http/Livewire/LayerContent.php
+++ app/Http/Livewire/LayerContent.php
@@ -7,4 +7,36 @@ use Livewire\Component;
 
 class LayerContent extends Component
 {
+    public $layerSlug;
+
+    protected $listeners = [
+        'layerChanged' => 'layerChanged'
+    ];
+
+    public function render()
+    {
+        $layer = Layer::firstWhere('slug', $this->layerSlug);
+
+        return view('livewire.layer-content')
+            ->with('layer', $layer)
+            ->with('parentLayers', $this->getParentLayers($layer));
+    }
+
+    public function layerChanged($layerSlug) {
+        $this->layerSlug = $layerSlug;
+    }
+
+    private function getParentLayers($layer) {
+        $parentLayers = [];
+
+        if ($layer != null) {
+            $parentLayer = $layer->parentLayer;
+            while($parentLayer != null) {
+                array_unshift($parentLayers, $parentLayer);
+                $parentLayer = $parentLayer->parentLayer;
+            }
+        }
+
+        return $parentLayers;
+    }
 }
--- resources/views/livewire/layer-content.blade.php
+++ resources/views/livewire/layer-content.blade.php
@@ -1,2 +1,26 @@
 <div class="container py-5 layer-content">
+    @if($layer != null)
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a onclick="window.Swapper.loadMap();" href="#">Home</a></li>
+                @foreach($parentLayers as $parentLayer)
+                    <li class="breadcrumb-item"><a onclick="window.Layer.load('{{ $parentLayer->slug }}');" href="#{{ $parentLayer->slug }}">{{ $parentLayer->name }}</a></li>
+                @endforeach
+                <li class="breadcrumb-item active" aria-current="page">{{ $layer->name }}</li>
+            </ol>
+        </nav>
+
+        <h1>{{ $layer->name }}</h1>
+
+        <div class="layer-content-text">{{ $layer->content }}</div>
+        <br/>
+
+        <div class="row">
+            @foreach($layer->childLayers as $childLayer)
+                <div class="col">
+                    <button onclick="window.Layer.load('{{ $childLayer->slug }}');" class="btn btn-outline-primary btn-layer">{{ $childLayer->name }}</button>
+                </div>
+            @endforeach
+        </div>
+    @endif
 </div>
