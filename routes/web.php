556808202748657b37e54373c9bf53f2d203ac89
Sander Jochems <sanderjochems@hotmail.nl>
2021-04-06 14:31:19 +0200

    Move Dashboard to Controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,6 +17,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home.index');
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+Route::prefix('admin')->middleware(['auth:sanctum', 'verified'])->name('admin.')->group(function () {
+    Route::get('', [DashboardController::class, 'index'])->name('dashboard');
+});
