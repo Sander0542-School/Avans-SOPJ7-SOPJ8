1c964ab515132fc8a980846eda6070c7ccaa97fa
Sander Jochems <sanderjochems@hotmail.nl>
2021-04-06 14:32:55 +0200

    Create Update for MenuController

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -17,5 +17,29 @@ class MenuController extends Controller
 
     public function update(UpdateRequest $request)
     {
+        $formSubjects = collect($request->input('subjects', []));
+        $errors = [];
+
+        foreach ($formSubjects as $formSubject) {
+            $subject = Subject::updateOrCreate([
+                'id' => $formSubject['id'],
+            ], [
+                'domain_id' => $formSubject['domain_id'],
+                'name' => $formSubject['name'],
+                'order' => $formSubject['order'],
+            ]);
+
+            if ($subject == null) {
+                $errors[] = __('admin.menu.errors.subject_not_created_updated', [
+                    'name' => $formSubject['name'],
+                ]);
+            }
+        }
+
+        Cache::forget('sidemenu');
+
+        return redirect()->back()->withErrors([
+            'menu' => $errors,
+        ]);
     }
 }
