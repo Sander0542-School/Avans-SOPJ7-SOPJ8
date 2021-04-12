c3ccaddab42e39b736ee8d14fcdc537998f60823
Sander Jochems <sanderjochems@hotmail.nl>
2021-04-12 13:26:30 +0200

    Update JS

--- resources/js/layer.js
+++ resources/js/layer.js
@@ -2,17 +2,17 @@ window.Layer = {
     load: (layerSlug) => {
         if (document.querySelector('.layer-content')) {
             window.location.hash = layerSlug;
+            console.log(Livewire);
             Livewire.emit('layerChanged', layerSlug);
             window.Swapper.loadContent();
             window.SideMenu.close();
         }
+    },
+    loadHash: () => {
+        if (window.location.hash) {
+            const layerSlug = window.location.hash.substr(1);
+            window.Layer.load(layerSlug);
+        }
     }
 }
 
-if (window.location.hash) {
-    const layerSlug = window.location.hash.substr(1);
-    window.Swapper.loadContent();
-    document.addEventListener("DOMContentLoaded", () => {
-        window.Layer.load(layerSlug);
-    });
-}
--- resources/js/subjectmap.js
+++ resources/js/subjectmap.js
@@ -3,44 +3,47 @@ const southWest = L.latLng(52.109024, 6.573585),
     northEast = L.latLng(52.123450, 6.616385),
     bounds = L.latLngBounds(southWest, northEast);
 
-const map = Leaflet.map('subjectmap', {
-    minZoom: 16,
-    maxZoom: 19,
-    zoomControl: false,
-    maxBounds: bounds,
-    attributionControl: false
-}).setView([52.115329, 6.596776], 16);
+window.SubjectMap = {
+    map: null,
+    renderMap: () => {
+        const map = Leaflet.map('subjectmap', {
+            minZoom: 16,
+            maxZoom: 19,
+            zoomControl: false,
+            maxBounds: bounds,
+            attributionControl: false
+        }).setView([52.115329, 6.596776], 16);
 
-Leaflet.tileLayer(layerTemplate, {
-    maxZoom: 19,
-    minZoom: 16
-}).addTo(map);
+        Leaflet.tileLayer(layerTemplate, {
+            maxZoom: 19,
+            minZoom: 16
+        }).addTo(map);
 
-window.subjectMap = map;
+        window.SubjectMap.map = map;
+    },
+    loadSubjects: (draggable = false) => {
+        fetch('/api/subjects').then(function (response) {
+            return response.json();
+        }).then(function (response) {
+            window.SubjectMap.placeMarkers(response.data, draggable);
+        })
+    },
+    placeMarkers: (subjects, draggable = false) => {
+        if (window.SubjectMap.map == null) return;
 
-getData();
-
-async function getData() {
-    let data = fetch('/api/subjects').then(function (response) {
-        return response.json();
-    }).then(function (obj) {
-        placeMarkers(obj['data'])
-    })
-}
-
-function placeMarkers(obj) {
-    obj.forEach(function (item) {
-        if (item.lon != null && item.lat != null) {
-            let marker = new L.marker([item.lon, item.lat], {
-                icon: new L.DivIcon({
-                    className: 'my-div-icon',
-                    html: '<div>' +
-                        '<img class="my-div-image" width="65" height="80" src="/images/MarkerImage.png"/>' +
-                        '<button class="btn btn-primary" style="text-align: center;">' + item.name + '</button>' +
-                        '</div>'
-                })
-            });
-            marker.addTo(map);
-        }
-    })
+        subjects.forEach(function (item) {
+            if (item.lon != null && item.lat != null) {
+                let marker = new L.marker([item.lon, item.lat], {
+                    icon: new L.DivIcon({
+                        className: 'my-div-icon',
+                        html: '<div>' +
+                            '<img class="my-div-image" width="65" height="80" src="/images/MarkerImage.png"/>' +
+                            '<button class="btn btn-primary" style="text-align: center;">' + item.name + '</button>' +
+                            '</div>'
+                    })
+                });
+                marker.addTo(window.SubjectMap.map);
+            }
+        });
+    }
 }
