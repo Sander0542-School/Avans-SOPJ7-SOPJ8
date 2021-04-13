33f740b76d7cc0195fd80a8f8d7516b2b9c9f924
Joren <63969493+joren961@users.noreply.github.com>
2021-04-13 17:15:52 +0200

    centered markers + color

--- resources/js/subjectmap.js
+++ resources/js/subjectmap.js
@@ -38,15 +38,23 @@ window.SubjectMap = {
     },
     placeMarkers: (subjects, draggable = false) => {
         if (window.SubjectMap.map == null) return;
-
         subjects.forEach(function (item) {
+            let button;
+            //veranderen naarmate meer domeinen komen
+            if(item.domain_id === 1) {
+                button='<button class="btn btn-primary" class="markerButton" style="background-color: #1976D2;border-color:#1976D2;">' + item.name + '</button>';
+            } else if (item.domain_id === 2) {
+                button = '<button class="btn btn-primary" class="markerButton" style="background-color: #D32F2F;border-color: #D32F2F;">' + item.name + '</button>';
+            } else {
+                button = '<button class="btn btn-primary" class="markerButton" style="background-color:#2E7D32;border-color: #2E7D32;">' + item.name + '</button>';
+            }
             let marker = new Leaflet.marker([item.lon, item.lat], {
                 draggable: draggable,
                 icon: new Leaflet.DivIcon({
                     className: 'my-div-icon',
-                    html: '<div>' +
+                    html: '<div class="marker-container">' +
                         '<img class="my-div-image" width="65" height="80" src="/images/MarkerImage.png"/>' +
-                        '<button class="btn btn-primary" style="text-align: center;">' + item.name + '</button>' +
+                        button +
                         '</div>'
                 }),
                 subjectId: item.id
