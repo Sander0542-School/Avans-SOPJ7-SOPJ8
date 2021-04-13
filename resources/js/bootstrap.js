c9bc02c266fd821c909923d61f80c6b33409bb94
Milanvanoeveren <63775330+Milanvanoeveren@users.noreply.github.com>
2021-04-13 22:03:26 +0200

    Update resources/js/bootstrap.js
    Co-authored-by: Sander Jochems <sanderjochems@hotmail.nl>

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -52,7 +52,5 @@ $( document ).ready(function() {
 });
 
 //jquery ui
-import $ from 'jquery';
-window.$ = window.jQuery = $;
 import 'jquery-ui/ui/widgets/autocomplete.js';
 import 'jquery-ui/ui/widgets/sortable.js';
