490027c33f4aa68d1f2408732ffe8f72a00beb34
Sander Jochems <sanderjochems@hotmail.nl>
2021-04-14 11:45:48 +0200

    Update cypress-api.php

--- cypress-api.php
+++ cypress-api.php
@@ -1,9 +1,9 @@
 <?php
 
-exec('cypress cache path', $cypressPath);
-exec('cypress version --component package', $cypressVersion);
+exec('npx cypress cache path', $cypressPath);
+exec('npx cypress version --component package', $cypressVersion);
 
-$appFile = $cypressPath.'/'.$cypressVersion.'/Cypress/resources/app/packages/server/config/app.yml';
+$appFile = $cypressPath[0].'/'.$cypressVersion[0].'/Cypress/resources/app/packages/server/config/app.yml';
 
 $appContent = file_get_contents($appFile);
 $appContent = str_replace('https://api.cypress.io/', getenv('CYPRESS_API_URL'), $appContent);
