8ed5e40d2b556992e6c886efd468bcec2622ec51
Sander Jochems <sanderjochems@hotmail.nl>
2021-04-23 23:46:06 +0200

    Update ManagerController.php

--- app/Http/Controllers/Admin/ManagerController.php
+++ app/Http/Controllers/Admin/ManagerController.php
@@ -6,48 +6,71 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\Manager\StoreRequest;
 use App\Http\Requests\Admin\Manager\UpdateRequest;
 use App\Models\User;
-use Illuminate\Http\Request;
+use Hash;
+use Password;
+use Spatie\Permission\Models\Role;
+use Str;
 
 class ManagerController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
     public function index()
     {
-        //
+        $managers = User::all();
+
+        return view('pages.admin.manager.index')->with('managers', $managers);
     }
 
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
     public function create()
     {
-        //
+        $roles = Role::all();
+
+        return view('pages.admin.manager.create')->with('roles', $roles);
     }
 
     /**
      * Store a newly created resource in storage.
      *
      * @param \App\Http\Requests\Admin\Manager\StoreRequest $request
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function store(StoreRequest $request)
     {
         $data = $request->validated();
+
+        $manager = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make(Str::random(12))
+        ]);
+
+        $manager->assignRole(Role::findById($data['role']));
+
+        $status = Password::sendResetLink($manager->only('email'));
+
+        if ($status === Password::RESET_LINK_SENT) {
+            return redirect()->route('admin.manager.index')->with(['status' => __($status)]);
+        }
+
+        return redirect()->back()->withErrors(['email' => __($status)]);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\User  $user
+     * @param \App\Models\User $manager
      * @return \Illuminate\Http\Response
      */
-    public function show(User $user)
+    public function show(User $manager)
     {
         //
     }
@@ -55,10 +78,10 @@ class ManagerController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\User  $user
+     * @param \App\Models\User $manager
      * @return \Illuminate\Http\Response
      */
-    public function edit(User $user)
+    public function edit(User $manager)
     {
         //
     }
@@ -67,10 +90,10 @@ class ManagerController extends Controller
      * Update the specified resource in storage.
      *
      * @param \App\Http\Requests\Admin\Manager\UpdateRequest $request
-     * @param \App\Models\User $user
+     * @param \App\Models\User $manager
      * @return \Illuminate\Http\Response
      */
-    public function update(UpdateRequest $request, User $user)
+    public function update(UpdateRequest $request, User $manager)
     {
         $data = $request->validated();
     }
@@ -78,10 +101,10 @@ class ManagerController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\User  $user
+     * @param \App\Models\User $manager
      * @return \Illuminate\Http\Response
      */
-    public function destroy(User $user)
+    public function destroy(User $manager)
     {
         //
     }
