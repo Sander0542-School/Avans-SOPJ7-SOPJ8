3836453cfb47fae505d9e6eb223addf9220ed703
Duwang <45719439+WhatABeatifulDuwang@users.noreply.github.com>
2021-04-29 18:29:19 +0200

    added route for managerController

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Admin\MapController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\MenuController;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -30,6 +31,15 @@ Route::prefix('admin')->middleware(['auth:sanctum', 'verified'])->name('admin.')
     });
 
     Route::resource('layers', LayerController::class)->only(['index', 'create', 'edit', 'store', 'update']);
+
+    Route::prefix('managers')->name('managers.')->group(function () {
+        Route::get('deleted', [ManagerController::class, 'deleted'])->name('deleted');
+        Route::post('restore', [ManagerController::class, 'restore'])->name('restore');
+    });
+
+    //Route::get('/managers/trashed-index', [ManagerController::class, 'trashedIndex'])->name('trashedIndex');
+    //Route::post('/managers/restore/{manager}', function(User $manager){dd('hoi', $manager);})
+    //Route::post('/managers/restore/{manager}',[ManagerController::class, 'restore'])->name('restore');
 
     Route::resource('managers', ManagerController::class);
 
