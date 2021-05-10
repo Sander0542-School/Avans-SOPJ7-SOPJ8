d35033a41d92ac1bd8e1a5e4e74de7ec3bcb7ea1
Andries van Rijn <46302364+avrijn@users.noreply.github.com>
2021-05-10 11:21:12 +0200

    Update app.php

--- config/app.php
+++ config/app.php
@@ -175,7 +175,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
-        App\Providers\FakerServiceProvider::class,
+        // App\Providers\FakerServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\CypressServiceProvider::class,
     ],
