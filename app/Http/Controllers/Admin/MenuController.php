3ec28f757addd806eb16062352fb5e60b9a92782
FedorTselishchev <ikbenfedor@gmail.com>
2021-06-02 13:58:45 +0200

    Revert "fix"
    This reverts commit 44bf88577314c7e88ad3099dbbda759326489bef.

--- app/Http/Controllers/Admin/MenuController.php
+++ app/Http/Controllers/Admin/MenuController.php
@@ -32,6 +32,7 @@ class MenuController extends Controller
                 'domain_id' => $formSubject['domain_id'],
                 'name' => $formSubject['name'],
                 'order' => $formSubject['order'],
+                'description' => $formSubject['description'],
             ]);
 
             if ($subject == null) {
