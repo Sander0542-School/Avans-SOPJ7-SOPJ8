f74c28c390a40e171f2d94c32f3e2678a2fdd394
Sander Jochems <sanderjochems@hotmail.nl>
2021-06-12 12:21:32 +0200

    Create layer_histories migration

---
+++ database/migrations/2021_06_12_121400_create_layer_histories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLayerHistoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('layer_histories', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('layer_id')->constrained('layers', 'id');
+            $table->string('action');
+            $table->string('name');
+            $table->string('slug');
+            $table->text('content');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('layer_histories');
+    }
+}
