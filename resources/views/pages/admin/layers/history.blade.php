64a658b42616effaeb0c3676d096dd90de9af965
Joren <63969493+joren961@users.noreply.github.com>
2021-06-21 15:33:19 +0200

    smallchanges

--- resources/views/pages/admin/layers/history.blade.php
+++ resources/views/pages/admin/layers/history.blade.php
@@ -1,8 +1,28 @@
 <x-app-layout>
-    <div>
-        <a class="btn btn-danger" href="{{ route('admin.layers.history-index',['change' => $change]) }}">Terug</a>
-    </div>
-    <div>
+    <x-slot name="header">
+        <h2 class="h4 font-weight-bold">
+            {{ __('Verandering') }}
+        </h2>
+    </x-slot>
 
+    <button type="button" class="btn btn-primary information" data-toggle="modal" data-target="#infoModal">
+        <i class="fa fa-info-circle my-float" ></i>
+    </button>
+    <div>
+        <h2>user:{{$user->name}} {{$change->action}} on: {{$change->updated_at}}</h2>
+        <div>
+            {{$layer->title}}
+        </div>
+
+        <div>
+            <span @if($change->action == "updated")
+                  class="yellow-content"
+                  @elseif($change->action == "deleted")
+                  class="red-content"
+                  @else
+                  class="green-content"
+                @endif>{{$change->content}}</span>
+        </div>
     </div>
+    <a class="btn btn-danger" href="{{ route('admin.layers.history-index',['layer'=>$layer]) }}">Terug</a>
 </x-app-layout>
